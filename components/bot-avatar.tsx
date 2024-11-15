cd0c65775e7272cc39b9b344a826a75498099b43
saitamar10 <94067189+saitamar10@users.noreply.github.com>
2024-11-15 22:14:27 +0300

    Update bot-avatar.tsx

--- components/bot-avatar.tsx
+++ components/bot-avatar.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarImage } from "@/components/ui/avatar";
 export const BotAvatar = () => {
   return (
     <Avatar className="h-12 w-12">
-      <AvatarImage className="p-1" src="/logo.png" alt="NeoMind logo" />
+      <AvatarImage className="p-1" src="/logo.png" alt="Zeki logo" />
     </Avatar>
   );
 };
